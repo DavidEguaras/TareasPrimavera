8ef39c54f3f0564fead306501620ab59461f595a
DavidEguaras <dabenegu@gmail.com>
2024-05-01 20:36:58 +0200

    PATCHPUT

--- API_Primavera/SERVIDOR/controllers/coches_controller.js
+++ API_Primavera/SERVIDOR/controllers/coches_controller.js
@@ -15,25 +15,6 @@ const getCoches = (req, res) => { //http://127.0.0.1:3000/coches
 }
 
 //metodo para obtener un registro por ID
-/*
-const getCocheById = (req, res) => { //http://127.0.0.1:3000/coches
-    const idRegistro = req.params.id;
-    db.query('SELECT * FROM coches WHERE id = ?,', [idRegistro], (err, resultados) => {
-        if (err) {
-            console.error('Error al obtener el registro');
-            res.status(500).json({error: 'Error interno del servidor'});
-        }else{
-            //verificar si se encontro el registro
-            if (resultados.length > 0) {
-                //devuelve el primer resultado
-                res.json(resultados[0]);
-            }else{
-                res.status(404).json({error: 'registro no encontrado'});
-            }
-        }
-        console.log();
-    });
-}*/
 const getCocheById = (req, res) =>{
    const idRegistro = req.params.id;
    db.query('SELECT * FROM coches WHERE id = ?', [idRegistro], (err, resultados)=>{
@@ -51,26 +32,50 @@ const getCocheById = (req, res) =>{
   })
 }
 
+//Metodo para crear un coche
+const crearCoche = (req,res)=>{
+    const {nuevoNombre, cantidad} = req.body;
+     // Insertar datos en la base de datos
+     db.query('INSERT INTO coches (nombre, cantidad) VALUES (?, ?)',
+                    [nuevoNombre, cantidad], (err, resultado) => {
+      if (err) {
+        console.error('Error al guardar datos en la base de datos:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        res.json({ recibido: true, nuevoNombre, cantidad, id: resultado.insertId });
+      }
+    });
+  };  
 
-const crearCoche = (req, res) => {
-    const {mensaje} = req.body;
-    res.json({        
-        recibido:true,
-         mensaje
+
+const patchCoche = (req, res) => {
+    const {nuevoNombre, cantidad} = req.body;
+    db.query('UPDATE coches SET nombre = ?, cantidad = ?',[nuevoNombre, cantidad], (err, resultado) => {
+        if (err) {
+          console.error('Error al guardar datos en la base de datos:', err);
+          res.status(500).json({ error: 'Error interno del servidor' });
+        } else {
+          res.json({ recibido: true, nuevoNombre, cantidad, id: resultado.insertId });
+        }
     });
 }
 
-// app.post('/api/enviar', (req, res) => {
-//     const {mensaje} = req.body;
-//     res.json({
-//         recibido:true,
-//         mensaje
-//     });
-// });
-
+const putCoche = (req, res) => {
+    const {nuevoNombre, cantidad} = req.body;
+    db.query('INSERT INTO coches (nombre, cantidad) VALUES (?,?)',[nuevoNombre, cantidad], (err, resultado) => {
+        if (err) {
+          console.error('Error al guardar datos en la base de datos:', err);
+          res.status(500).json({ error: 'Error interno del servidor' });
+        } else {
+          res.json({ recibido: true, nuevoNombre, cantidad, id: resultado.insertId });
+        }
+    });
+}
 
 module.exports={
     getCoches,
     crearCoche,
-    getCocheById
+    getCocheById,
+    patchCoche,
+    putCoche
 }
--- API_Primavera/SERVIDOR/rutas/rutas_coches.js
+++ API_Primavera/SERVIDOR/rutas/rutas_coches.js
@@ -13,5 +13,12 @@ rutasCoches.post('/', cochesController.crearCoche);
 //ruta para obtener un coche por id
 rutasCoches.get('/:id', cochesController.getCocheById);
 
+//ruta para modificar parcialmente un registro de un coche
+rutasCoches.patch('/', cochesController.patchCoche);
+
+//ruta para modificar por completo el registro de un coche
+rutasCoches.put('/', cochesController.putCoche);
+
+
 module.exports = rutasCoches;
 
