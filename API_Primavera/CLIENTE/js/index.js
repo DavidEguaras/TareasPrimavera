f162c6ce334e0e456da8f61d834767e7e9ef2721
DavidEguaras <dabenegu@gmail.com>
2024-05-03 21:10:08 +0200

    getCocheByID

--- API_Primavera/CLIENTE/js/index.js
+++ API_Primavera/CLIENTE/js/index.js
@@ -5,7 +5,7 @@ const url=`http://${dirIP_api}:${PUERTO_EXPRESS}`;
 
 
 //--------------------PETICIONES GET------------------------
-
+//Funcion para obtener todos los coches
 function getCoches(){
     fetch(url+'/coches')
         .then(datosCrudos => {
@@ -19,11 +19,30 @@ function getCoches(){
         y se lo pasamos a la ul con id 'todosLosCoches'*/
         .then(datosObjeto => {
             const listaCoches = datosObjeto.map(coche => `<li>${coche.nombre} - ${coche.cantidad}</li>`).join('');
-            document.getElementById('todosLosCoches').innerHTML = listaCoches;
+            document.getElementById('todosLosCoches').innerHTML += listaCoches;
             console.log(datosObjeto);
         })
         .catch(error=>console.log(error));
 }
+
+//Evento para obtener un coche o coches por su id
+document.getElementById('getCocheByIDForm').addEventListener('submit', (event) =>{
+    event.preventDefault();
+    const idCoche = document.getElementById('idCoche').value;
+    fetch(url+'/coches/'+ idCoche)
+        .then(datosCrudos=>{
+            if(!datosCrudos.ok){
+                throw `Cuidado: ${datosCrudos.status}: ${datosCrudos.statusText}`;
+            }
+            console.log(datosCrudos);
+            return datosCrudos.json();
+        })
+        .then(datosObjeto=>{
+            document.getElementById('cocheID').innerHTML = datosObjeto.nombre+" - "+datosObjeto.cantidad;
+            console.log(datosObjeto);
+        })
+        .catch(error=>console.log(error));
+})
 
 //--------------------PETICIONES GET------------------------
 
